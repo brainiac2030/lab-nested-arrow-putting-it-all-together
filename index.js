b352c9fe1982c6ab7c41a51d8c0c16ed6f03c4c4
brainiac2030 <isaackiambo@gmail.com>
2026-03-27 21:16:30 +0300

    I added a secure login feature

--- index.js
+++ index.js
@@ -4,3 +4,36 @@
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
 };
+
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+
+  const login = (passwordAtempt) => {
+    attemptCount++;
+
+    if (attemptCount > 3) {
+      return "Account locked dued to too many faild login attempts";
+    }
+
+    if (passwordAtempt === userInfo.password) {
+      return "Login successful";
+    } 
+
+    return `Login failed. Attempt ${attemptCount}`;
+  };
+
+  return login;
+}
+
+
+const user = {
+  username:"john",
+  password: "1234"
+};
+
+const login = createLoginTracker(user);
+
+console.log(login("1111"));
+console.log(login("2222"));
+console.log(login("3333"));
+console.log(login("1234"));
